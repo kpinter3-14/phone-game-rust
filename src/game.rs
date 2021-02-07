73065570e438d60059302105bbde65de7b10ce88
Krisztian Pinter <kpinter3.14@gmail.com>
2021-02-08 00:25:03 +0100

    use floats internally for positions

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,6 @@ use crate::game_lib::types::*;
 use crate::game_lib::*;
 use rand::prelude::*;
 use sdl2::event::Event;
-use sdl2::rect::Rect;
 
 pub fn init(gcontext: &mut GContext) {
   #[rustfmt::skip]
@@ -34,19 +33,19 @@ pub fn init(gcontext: &mut GContext) {
 }
 
 pub struct State {
-  pub rng: rand::prelude::ThreadRng,
+  rng: rand::prelude::ThreadRng,
 
-  pub paddle_rect: sdl2::rect::Rect,
-  pub paddle_dir: V2I,
+  paddle_pos: P2F,
+  paddle_dir: f32,
 
-  pub ball_rect: sdl2::rect::Rect,
-  pub ball_dir: V2I,
+  ball_pos: P2F,
+  ball_dir: V2F,
 
-  pub fruits: Vec<sdl2::rect::Rect>,
-  pub score: i32,
+  fruits: Vec<Rect>,
+  score: i32,
 }
 
-const BALL_SPEED: i32 = 1;
+const BALL_SPEED: f32 = 1.0;
 const BALL_SIZE: u32 = 6;
 const FRUIT_SPEED: i32 = 2;
 const FRUIT_SIZE: u32 = 8;
@@ -58,11 +57,11 @@ impl State {
     State {
       rng: rand::thread_rng(),
 
-      paddle_rect: Rect::new(1, 1, PADDLE_SIZE.x, PADDLE_SIZE.y),
-      paddle_dir: V2I::new(0, 0),
+      paddle_pos: P2F::new(1.0, 1.0),
+      paddle_dir: 0.0,
 
-      ball_rect: Rect::new(0, 20, BALL_SIZE, BALL_SIZE),
-      ball_dir: V2I::new(BALL_SPEED, BALL_SPEED),
+      ball_pos: P2F::new(0.0, 20.0),
+      ball_dir: V2F::new(BALL_SPEED, BALL_SPEED),
 
       fruits: Vec::new(),
       score: 0,
@@ -73,58 +72,69 @@ impl State {
 pub fn update(state: State, game_tick_counter: i32) -> State {
   let mut state = state;
   // update paddle
-  state.paddle_rect.y += state.paddle_dir.y;
-  if state.paddle_rect.y < 1 {
-    state.paddle_rect.y = 1;
-    state.paddle_dir.y = 0;
+  state.paddle_pos.y += state.paddle_dir;
+  if state.paddle_pos.y < 1.0 {
+    state.paddle_pos.y = 1.0;
+    state.paddle_dir = 0.0;
   }
-  if state.paddle_rect.y + PADDLE_SIZE.y as i32 > 47 {
-    state.paddle_rect.y = 48 - 1 - PADDLE_SIZE.y as i32;
-    state.paddle_dir.y = 0;
+  if state.paddle_pos.y + PADDLE_SIZE.y as f32 > 47.0 {
+    state.paddle_pos.y = 48.0 - 1.0 - PADDLE_SIZE.y as f32;
+    state.paddle_dir = 0.0;
   }
 
   // update ball
-  state.ball_rect.x += state.ball_dir.x;
-  state.ball_rect.y += state.ball_dir.y;
-  if state.ball_rect.x < 0 {
+  state.ball_pos.x += state.ball_dir.x;
+  state.ball_pos.y += state.ball_dir.y;
+  if state.ball_pos.x < 0.0 {
     // TODO game over
     state = State::new();
   }
-  if state.ball_rect.x + BALL_SIZE as i32 >= 84 {
+  if state.ball_pos.x + BALL_SIZE as f32 >= 84.0 {
     state.ball_dir.x = -BALL_SPEED;
   }
-  if state.ball_rect.y < 0 {
+  if state.ball_pos.y < 0.0 {
     state.ball_dir.y = BALL_SPEED;
   }
-  if state.ball_rect.y + BALL_SIZE as i32 >= 48 {
+  if state.ball_pos.y + BALL_SIZE as f32 >= 48.0 {
     state.ball_dir.y = -BALL_SPEED;
   }
-  let ball_rect = Rect::new(state.ball_rect.x, state.ball_rect.y, BALL_SIZE, BALL_SIZE);
-  if state.paddle_rect.intersection(ball_rect).is_some() {
+  let ball_rect = Rect::new(
+    state.ball_pos.x,
+    state.ball_pos.y,
+    BALL_SIZE as f32,
+    BALL_SIZE as f32,
+  );
+  let paddle_rect = Rect::new(
+    state.paddle_pos.x,
+    state.paddle_pos.y,
+    PADDLE_SIZE.x as f32,
+    PADDLE_SIZE.y as f32,
+  );
+  if paddle_rect.intersects(&ball_rect) {
     state.ball_dir.x = BALL_SPEED;
   }
 
   // update fruits
   if game_tick_counter % 14 == 0 {
     state.fruits.push(Rect::new(
-      state.rng.gen_range(10..80),
-      -5,
-      FRUIT_SIZE,
-      FRUIT_SIZE,
+      state.rng.gen_range(10..80) as f32,
+      -5.0,
+      FRUIT_SIZE as f32,
+      FRUIT_SIZE as f32,
     ));
   }
   for fruit in &mut state.fruits {
-    fruit.y += FRUIT_SPEED;
+    fruit.y += FRUIT_SPEED as f32;
   }
   state.score += state
     .fruits
     .iter()
-    .filter(|&fruit| ball_rect.intersection(*fruit).is_some())
+    .filter(|&fruit| ball_rect.intersects(fruit))
     .count() as i32;
   state.fruits = state
     .fruits
     .iter()
-    .filter(|&fruit| ball_rect.intersection(*fruit).is_none() && fruit.y < 50)
+    .filter(|&fruit| !ball_rect.intersects(fruit) && fruit.y < 50.0)
     .map(|x| *x)
     .collect();
 
@@ -133,12 +143,12 @@ pub fn update(state: State, game_tick_counter: i32) -> State {
 
 pub fn render(gcontext: &mut GContext, state: &State) {
   for fruit in &state.fruits {
-    gcontext.draw_sprite(fruit.x, fruit.y, "cherry");
+    gcontext.draw_sprite(fruit.x as i32, fruit.y as i32, "cherry");
   }
-  gcontext.draw_sprite(state.ball_rect.x, state.ball_rect.y, "ball");
+  gcontext.draw_sprite(state.ball_pos.x as i32, state.ball_pos.y as i32, "ball");
   gcontext.draw_dark_rect(
-    state.paddle_rect.x,
-    state.paddle_rect.y,
+    state.paddle_pos.x as i32,
+    state.paddle_pos.y as i32,
     PADDLE_SIZE.x,
     PADDLE_SIZE.y,
   );
@@ -151,7 +161,7 @@ pub fn handle_event(state: State, event: &sdl2::event::Event) -> State {
       keycode: Some(keycode),
       ..
     } => handle_keypress(state, keycode),
-    _ => state
+    _ => state,
   }
 }
 
@@ -159,17 +169,17 @@ fn handle_keypress(state: State, keycode: sdl2::keyboard::Keycode) -> State {
   let mut state = state;
   match keycode {
     sdl2::keyboard::Keycode::W => {
-      if state.paddle_dir.y >= 0 {
-        state.paddle_dir.y = -PADDLE_SPEED;
+      if state.paddle_dir >= 0.0 {
+        state.paddle_dir = -PADDLE_SPEED as f32;
       } else {
-        state.paddle_dir.y = 0;
+        state.paddle_dir = 0.0;
       }
     }
     sdl2::keyboard::Keycode::S => {
-      if state.paddle_dir.y <= 0 {
-        state.paddle_dir.y = PADDLE_SPEED;
+      if state.paddle_dir <= 0.0 {
+        state.paddle_dir = PADDLE_SPEED as f32;
       } else {
-        state.paddle_dir.y = 0;
+        state.paddle_dir = 0.0;
       }
     }
     _ => {}
--- src/game_lib.rs
+++ src/game_lib.rs
@@ -16,6 +16,26 @@ pub mod types {
   pub type P4U8 = cgmath::Vector4<u8>;
   pub type M4F = cgmath::Matrix4<f32>;
   pub type Store<T> = std::collections::HashMap<String, T>;
+
+  #[derive(Copy, Clone)]
+  pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+  }
+
+  impl Rect {
+    pub fn new(x: f32, y: f32, w: f32, h: f32) -> Rect {
+      Rect { x, y, w, h }
+    }
+
+    pub fn intersects(&self, other: &Rect) -> bool {
+      let x_overlaps = !(self.x > other.x + other.w || self.x + self.w < other.x);
+      let y_overlaps = !(self.y > other.y + other.h || self.y + self.h < other.y);
+      x_overlaps && y_overlaps
+    }
+  }
 }
 
 use types::*;
